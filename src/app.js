948d911aa9a8b114fd8a6e677d91d9c9343f14e1
dockeruser881 <ketan.ray@rmoneyindia.com>
2026-03-03 11:17:34 +0530

    Feature : Added MongoDB and Helathcheck

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
+const config = require("./config/env");
+const connectDB = require("./config/database");
 
 const app = express();
 
 app.use(express.json());
 
+connectDB();
+
+const mongoose = require("mongoose");
+
 app.get("/health", (req, res) => {
-  res.json({ status: "OK" });
+  const dbStatus =
+    mongoose.connection.readyState === 1 ? "UP" : "DOWN";
+
+  res.json({
+    status: "OK",
+    database: dbStatus,
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-
-const userRoutes = require("./routes/user.routes");
-app.use("/api", userRoutes);
+const PORT = config.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
